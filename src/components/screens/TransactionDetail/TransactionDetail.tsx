5f015f6c606738905a24aa264d4fada6c1c31ca4
Grassper <rishi.bharath27@gmail.com>
2021-08-07 11:41:43 +0530

    feat: :sparkles: Added transaction details pages

--- src/components/screens/TransactionDetail/TransactionDetail.tsx
+++ src/components/screens/TransactionDetail/TransactionDetail.tsx
@@ -1,16 +1,14 @@
 import {MaterialIcons} from '@expo/vector-icons';
 import {StackNavigationProp} from '@react-navigation/stack';
-import React from 'react';
-import {Dimensions} from 'react-native';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 
 import {BackButton} from '@/root/src/components/shared/Button';
 import {
-    Container,
-    HeadingContainer
+    HeadingContainer,
+    ScrollContainer
 } from '@/root/src/components/shared/Container';
 import {Header} from '@/root/src/components/shared/Header';
-import {HeadingText} from '@/root/src/components/shared/HeadingText';
 import Colors from '@/root/src/constants/colors';
 
 type RootStackParamList = {
@@ -63,40 +61,143 @@ const TitleText = styled(Text)`
     font-family: 'rm';
 `;
 
+const FieldHeading = styled(Text)`
+    font-size: 16px;
+    margin-bottom: 20px;
+`;
+
+const FieldText = styled(Text)`
+    font-size: 16px;
+    font-family: 'mr';
+    color: ${Colors.primary};
+    background-color: ${Colors.white};
+    line-height: 28px;
+    border-radius: 5px;
+    padding: 10px;
+    margin-bottom: 20px;
+`;
+
 const SubHeadingContainer = styled.View`
     flex-direction: row;
     justify-content: space-between;
     width: 100%;
     margin-top: -10px;
+    padding-bottom: 150px;
+`;
+
+const Container = styled.View`
+    background-color: ${Colors.pink};
+    align-items: center;
+`;
+
+const ContentContainer = styled.View`
+    flex: 1;
+    background-color: ${Colors.background};
+    align-items: center;
+`;
+
+const AppContainer = styled.View`
+    width: 90%;
+`;
+
+const ImageContainer = styled.View`
+    margin-bottom: 20px;
+    height: 150px;
+    flex: auto;
+    border-radius: 7px;
+    overflow: hidden;
+`;
+
+const Image = styled.Image`
+    width: 100%;
+    height: 100%;
+    border-radius: 7px;
+    background-color: ${Colors.white};
+`;
+
+const DetailsBox = styled.View`
+    background-color: ${Colors.darkGray};
+    width: 100%;
+    padding: 20px;
+    border-radius: 10px;
+    margin-top: -120px;
 `;
 
 export const TransactionDetail: React.FC<Props> = ({navigation}) => {
+    const [imageUrl, setImageUrl] = useState(
+        'https://images.pexels.com/' +
+            'photos/1602726/pexels-photo-1602726.jpeg'
+    );
     return (
-        <Container backgroundColor={Colors.pink}>
-            <Header>
-                <BackButton onClick={() => navigation.goBack()} />
-                <IconContainer>
-                    <MaterialIcons
-                        name="mode-edit"
-                        size={26}
-                        color={Colors.white}
-                    />
-                    <Spacer />
-                    <MaterialIcons
-                        name="delete"
-                        size={26}
-                        color={Colors.white}
-                    />
-                </IconContainer>
-            </Header>
-            <HeadingContainer header>
-                <TitleText>Swiggy</TitleText>
-                <CurrencyText>$ 3,040</CurrencyText>
-            </HeadingContainer>
-            <SubHeadingContainer>
-                <CategoryText>Food</CategoryText>
-                <DateText>30 oct 2021, 1.35 pm</DateText>
-            </SubHeadingContainer>
-        </Container>
+        <>
+            <Container>
+                <AppContainer>
+                    <Header>
+                        <BackButton onClick={() => navigation.goBack()} />
+                        <IconContainer>
+                            <MaterialIcons
+                                name="mode-edit"
+                                size={26}
+                                color={Colors.white}
+                            />
+                            <Spacer />
+                            <MaterialIcons
+                                name="delete"
+                                size={26}
+                                color={Colors.white}
+                            />
+                        </IconContainer>
+                    </Header>
+                    <HeadingContainer header>
+                        <TitleText>Swiggy</TitleText>
+                        <CurrencyText>$ 3,040</CurrencyText>
+                    </HeadingContainer>
+                    <SubHeadingContainer>
+                        <CategoryText>Food</CategoryText>
+                        <DateText>30 oct 2021, 1.35 pm</DateText>
+                    </SubHeadingContainer>
+                </AppContainer>
+            </Container>
+            <ContentContainer>
+                <AppContainer>
+                    <DetailsBox>
+                        <ScrollContainer>
+                            <FieldHeading>Transfer Type</FieldHeading>
+                            <FieldText>Indian bank 1339</FieldText>
+                            <FieldHeading>Description</FieldHeading>
+                            <FieldText>
+                                Sed a magna semper, porta purus eu, ullamcorper
+                                ligula. Nam sit amet consectetur sapien. Etiam
+                                dui ipsum, viverra vel turpis ut, dignissim
+                                elementum mauris.
+                            </FieldText>
+                            <FieldHeading>Message</FieldHeading>
+                            <FieldText>
+                                Sed a magna semper, porta purus eu, ullamcorper
+                                ligula. Nam sit amet consectetur sapien. Etiam
+                                dui ipsum, viverra vel turpis ut, dignissim
+                                elementum mauris. Sed dapibus auctor
+                                scelerisque. Aenean at leo tellus.
+                            </FieldText>
+                            {!!imageUrl && (
+                                <>
+                                    <FieldHeading>
+                                        Bill Or Warranty
+                                    </FieldHeading>
+                                    <ImageContainer>
+                                        <Image
+                                            source={{
+                                                uri: imageUrl
+                                            }}
+                                            accessibilityLabel="bill image"
+                                        />
+                                    </ImageContainer>
+                                </>
+                            )}
+                        </ScrollContainer>
+                    </DetailsBox>
+                </AppContainer>
+            </ContentContainer>
+        </>
     );
 };
